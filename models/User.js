55fe75442d7b3ac4731c76b99c5184c1e0e4db83
Mario Lopez <jlopezvillarreal@my.bcit.ca>
2022-12-02 10:09:54 -0800

    added login and register

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const passportLocalmongoose = require("passport-local-mongoose");
+
+// User Schema
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    index: true,
+  },
+  email: {
+    type: String,
+    index: true,
+  },
+  password: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  roles: {
+    type: Array,
+  },
+  interests: {
+    type: Array
+  },
+  imagePath: {
+    type: String
+  }
+});
+
+userSchema.plugin(passportLocalmongoose);
+
+// Pass the Schema into Mongoose to use as our model
+const User = mongoose.model("User", userSchema);
+
+// Export it so that we can use this model in our App
+module.exports = User;
